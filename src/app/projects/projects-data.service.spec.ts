fd69470708817f4e36ea65f3dfb224f7376a0497
MiguelFernandes-BSimple <miguel.fernandes@b-simple.pt>
2025-05-27 09:25:27 +0100

    projects tests

--- src/app/projects/projects-data.service.spec.ts
+++ src/app/projects/projects-data.service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ProjectsDataService } from './projects-data.service';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('ProjectsDataService', () => {
   let service: ProjectsDataService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
+    });
     service = TestBed.inject(ProjectsDataService);
   });
 
