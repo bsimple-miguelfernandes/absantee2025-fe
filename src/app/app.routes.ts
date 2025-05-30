712aa9ddf3aad831013d226f0f150f546098d993
Fernando Silva <fernandosilvajfgs@gmail.com>
2025-05-30 16:13:18 +0100

    add training modules component and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { AppComponent } from './app.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { HomeComponent } from './home/home.component';
 import { CollaboratorsComponent } from './collaborators/collaborators.component';
+import { TrainingModulesComponent } from './trainingModules/training-modules/training-modules.component';
 
 export const routes: Routes = [
     {
@@ -16,5 +17,9 @@ export const routes: Routes = [
     {
         path: 'collaborators',
         component: CollaboratorsComponent
+    },
+    {
+        path: 'trainingModules',
+        component: TrainingModulesComponent
     }
 ];
---
+++ src/app/trainingModules/training-modules/training-modules.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-training-modules',
+  imports: [],
+  templateUrl: './training-modules.component.html',
+  styleUrl: './training-modules.component.css'
+})
+export class TrainingModulesComponent {
+
+}
