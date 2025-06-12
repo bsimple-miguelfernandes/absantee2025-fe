0856c986f907cec59d209fad7e89f2fb33d476eb
Fernando Silva <fernandosilvajfgs@gmail.com>
2025-06-12 01:53:34 +0100

    add filter to projects

--- src/app/projects/projects-table/projects-table.component.ts
+++ src/app/projects/projects-table/projects-table.component.ts
@@ -1,14 +1,31 @@
 import { Component, input } from '@angular/core';
 import { Project } from '../project/project';
 import { RouterModule } from '@angular/router';
+import { FiltersComponent } from "../../filters/filters.component";
 
 @Component({
   selector: 'app-projects-table',
-  imports: [RouterModule],
+  imports: [RouterModule, FiltersComponent],
   templateUrl: './projects-table.component.html',
   styleUrl: './projects-table.component.css'
 })
 export class ProjectsTableComponent {
   projects = input.required<Project[]>();
+
+  filteredProjects: Project[] = [];
+
+  ngOnChanges() {
+    this.filteredProjects = this.projects();
+  }
+
+  applyFilters(filters: Record<string, string>) {
+    const title = filters['title']?.toLowerCase() ?? '';
+    const acronym = filters['acronym']?.toLowerCase() ?? '';
+
+    this.filteredProjects = this.projects().filter(project =>
+      (!title || project.title.toLowerCase().includes(title)) &&
+      (!acronym || project.acronym.toLowerCase().includes(acronym))
+    )
+  }
 
 }
