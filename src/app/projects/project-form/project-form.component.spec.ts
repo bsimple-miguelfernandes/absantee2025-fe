d01cfd6ff97c57370ddfde5cd58c2d6ec2707411
josemalheiro <josemalheiro@BSPC2446>
2025-06-13 17:03:02 +0100

    project data service tests done

--- src/app/projects/project-form/project-form.component.spec.ts
+++ src/app/projects/project-form/project-form.component.spec.ts
@@ -1,43 +1,43 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { ProjectFormComponent } from './project-form.component';
-// import { ProjectsSignalsService } from '../projects-signals.service';
-// import { ProjectsDataService } from '../projects-data.service';
-// import { ActivatedRoute } from '@angular/router';
-// import { of } from 'rxjs';
+import { ProjectFormComponent } from './project-form.component';
+import { ProjectsSignalsService } from '../projects-signals.service';
+import { ProjectsDataService } from '../projects-data.service';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
-// describe('ProjectFormComponent', () => {
-//   let component: ProjectFormComponent;
-//   let fixture: ComponentFixture<ProjectFormComponent>;
-//   let mockProjectSignalService: jasmine.SpyObj<ProjectsSignalsService>;
-//   let mockProjectDataService: jasmine.SpyObj<ProjectsDataService>;
-//   beforeEach(async () => {
+describe('ProjectFormComponent', () => {
+    let component: ProjectFormComponent;
+    let fixture: ComponentFixture<ProjectFormComponent>;
+    let mockProjectSignalService: jasmine.SpyObj<ProjectsSignalsService>;
+    let mockProjectDataService: jasmine.SpyObj<ProjectsDataService>;
+    beforeEach(async () => {
 
-//     mockProjectDataService = jasmine.createSpyObj('ProjectDataService', ['']);
-//     await TestBed.configureTestingModule({
-//       imports: [ProjectFormComponent],
-//       providers: [
-//         { provide: ProjectsDataService, useValue: mockProjectDataService },
-//         {
-//           provide: ActivatedRoute,
-//           useValue: {
-//             snapshot: {},
-//             params: of({}),
-//             queryParams: of({}),
-//             url: of([]),
-//             data: of({})
-//           }
-//         }
-//       ]
-//     })
-//       .compileComponents();
+        mockProjectDataService = jasmine.createSpyObj('ProjectDataService', ['']);
+        await TestBed.configureTestingModule({
+            imports: [ProjectFormComponent],
+            providers: [
+                { provide: ProjectsDataService, useValue: mockProjectDataService },
+                {
+                    provide: ActivatedRoute,
+                    useValue: {
+                        snapshot: {},
+                        params: of({}),
+                        queryParams: of({}),
+                        url: of([]),
+                        data: of({})
+                    }
+                }
+            ]
+        })
+            .compileComponents();
 
-//     fixture = TestBed.createComponent(ProjectFormComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+        fixture = TestBed.createComponent(ProjectFormComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
--- src/app/projects/projects-data.service.spec.ts
+++ src/app/projects/projects-data.service.spec.ts
@@ -4,6 +4,9 @@ import { ProjectsDataService } from './projects-data.service';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { Project } from './models/project.model';
+import { AssociationProjectCollaboratorsDTO } from '../associations-project-collaborator/association-project-collaborator.model';
+import { ProjectCreateRequest } from './models/create-project.model';
+import { AssociationProjectCollaboratorCreateRequest } from '../associations-project-collaborator/add-collaborator-project/add-association';
 
 describe('ProjectsDataService', () => {
   let service: ProjectsDataService;
@@ -26,6 +29,7 @@ describe('ProjectsDataService', () => {
   afterEach(() => httpMock.verify());
 
   it('should fetch all projects', (() => {
+    // Arrange
     const mockProjects: Project[] = [
       {
         id: '1',
@@ -42,14 +46,17 @@ describe('ProjectsDataService', () => {
 
     service.getProjects().subscribe(r => (result = r));
 
+    // Act
     const req = httpMock.expectOne('http://localhost:5073/api/Project');
     expect(req.request.method).toBe('GET');
     req.flush(mockProjects);
 
+    // Assert
     expect(result).toEqual(mockProjects);
   }));
 
   it('should fetch project by id', (() => {
+    // Arrange
     const mockProject: Project = {
       id: '1',
       title: 'Project 1',
@@ -63,38 +70,112 @@ describe('ProjectsDataService', () => {
     let result!: Project;
     service.getProjectById('1').subscribe(p => (result = p));
 
+    // Act
     const req = httpMock.expectOne('http://localhost:5073/api/Project/1');
     expect(req.request.method).toBe('GET');
     req.flush(mockProject);
 
-
+    // Assert
     expect(result).toEqual(mockProject);
   }));
 
+  it('should fetch associations for a project', () => {
+    // Arrange
+    const id = '1';
+    const mockDTOs: AssociationProjectCollaboratorsDTO[] = [
+      {
+        id: '1',
+        projectId: '1',
+        projectAcronym: 'um',
+        collaboratorId: '1',
+        collaboratorEmail: 'um@email.com',
+        periodDate: {
+          initDate: new Date(),
+          finalDate: new Date()
+        }
+      }
+    ];
 
-  // it('should fetch associations by id', (() => {
-  //   const mockAssociations: AssociationProjectCollaborators[] = [
-  //     {
-  //       id: "1",
-  //       projectId: "1",
-  //       projectAcronym: "T1",
-  //       collaboratorId: "1",
-  //       collaboratorEmail: "test1@example.com",
-  //       periodDate: {
-  //         initDate : new Date('2024-01-01'),
-  //         finalDate: new Date('2024-12-31')
-  //       }
-  //     }
-  //   ];
+    service.getAssociations(id).subscribe(assocs => {
+      expect(assocs).toEqual(mockDTOs);
+    });
 
-  //   let result!: AssociationProjectCollaborators[];
-  //   service.getAssociations('1').subscribe(p => (result = p));
+    // Act
+    const req = httpMock.expectOne(`http://localhost:5073/api/Project/${id}/associations`);
 
-  //   const req = httpMock.expectOne('http://localhost:5073/api/Project/1/associations');
-  //   expect(req.request.method).toBe('GET');
-  //   req.flush(mockAssociations);
+    // Assert
+    expect(req.request.method).toBe('GET');
+    req.flush(mockDTOs);
+  });
 
-  //   
-  //   expect(result).toEqual(mockAssociations);
-  // }));
+  it('should create a new project', () => {
+    const newProject: ProjectCreateRequest = {
+      title: 'New', acronym: 'N',
+      periodDate: {
+        initDate: new Date().toDateString(),
+        finalDate: new Date().toDateString()
+      }
+    };
+    const createdProject: Project = {
+      title: 'um',
+      acronym: 'UM',
+      periodDate: {
+        initDate: new Date(),
+        finalDate: new Date()
+      }
+    };
+
+    service.createProject(newProject).subscribe(result => {
+      expect(result).toEqual(createdProject);
+    });
+
+    const req = httpMock.expectOne(`http://localhost:5073/api/Project`);
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual(newProject);
+    req.flush(createdProject);
+  });
+
+  it('should update a project', () => {
+    const updatedProject: Project = { id: '2', title: 'Updated Project' } as Project;
+
+    service.updateProject(updatedProject).subscribe(result => {
+      expect(result).toEqual(updatedProject);
+    });
+
+    const req = httpMock.expectOne(`http://localhost:5073/api/Project`);
+    expect(req.request.method).toBe('PUT');
+    expect(req.request.body).toEqual(updatedProject);
+    req.flush(updatedProject);
+  });
+
+  it('should create a collaborator association', () => {
+    const id = 'project1';
+    const newAssoc: AssociationProjectCollaboratorCreateRequest = {
+      collaboratorId: '1',
+      periodDate: {
+        initDate: new Date(),
+        finalDate: new Date()
+      }
+    };
+    const response: AssociationProjectCollaboratorsDTO = {
+      id: 'assoc1',
+      projectId: id,
+      projectAcronym: 'um',
+      collaboratorId: 'collab1',
+      collaboratorEmail: 'um@email.com',
+      periodDate: {
+        initDate: new Date(),
+        finalDate: new Date()
+      }
+    };
+
+    service.createAssociation(id, newAssoc).subscribe(result => {
+      expect(result).toEqual(response);
+    });
+
+    const req = httpMock.expectOne(`http://localhost:5073/api/Project/${id}/collaborators`);
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual(newAssoc);
+    req.flush(response);
+  });
 });
